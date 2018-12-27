29d659baeb5c96d4fc1ef7ea3f1b2acd96a7844c
John Tucker <jtucker@appirio.com>
2018-12-27 15:23:56 -0500

    Linting

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
@@ -13,9 +13,9 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
   },
 });
